8a0cdc93b360559fb8d5e84886dde7032e3195cd
ecjourdan <ejourdan14@gmail.com>
2025-05-23 10:14:13 +0100

    first commit

--- 01. Static pages/02. What we'll learn/index.jsx
+++ 01. Static pages/02. What we'll learn/index.jsx
@@ -1,5 +1,11 @@
+
 import { createRoot } from "react-dom/client"
+
+//1. Create a root
 import App from "./App"
 
+//2. Redner some markup to the root
+
 const root = createRoot(document.getElementById("root"))
-root.render(<App />)
+root.render(<h1>Hello, React</h1>)
+
